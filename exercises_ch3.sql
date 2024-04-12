2759fd36778da7d40fe65a5b243264e8650a323f
blazej32 <blazej.klepacki1@gmail.com>
2024-04-12 23:15:36 +0200

    finish exercise 3-3

--- exercises_ch3.sql
+++ exercises_ch3.sql
@@ -7,3 +7,8 @@ ORDER BY last_name, first_name;
 SELECT actor_id, first_name, last_name
 FROM actor
 WHERE last_name = 'WILLIAMS' or last_name = 'DAVIS';
+
+-- exercise 3-3
+SELECT DISTINCT customer_id
+FROM rental
+WHERE date(rental_date) = '2005-07-05';
