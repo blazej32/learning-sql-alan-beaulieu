088433c791b22d83f8a34b831ef28628b62f7801
blazej32 <blazej.klepacki1@gmail.com>
2024-04-15 14:00:03 +0200

    finish exercises for chapter 7

---
+++ exercises_ch7.sql
@@ -0,0 +1,8 @@
+-- exercise 7-1
+SELECT SUBSTRING('Please find the substring in this string', 17, 9);
+
+-- exercise 7-2
+SElECT ABS(-25.76823), SIGN(-25.76823), ROUND(-25.76823, 2);
+
+-- exercise 7-3
+SELECT EXTRACT(MONTH FROM CURRENT_DATE());
