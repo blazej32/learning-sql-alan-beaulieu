237b7fc3711d157c89464b774aa208fe7cafb6f4
blazej32 <blazej.klepacki1@gmail.com>
2024-04-16 16:16:12 +0200

    finish exercises for chapter 8

---
+++ exercises_ch8.sql
@@ -0,0 +1,14 @@
+-- exercise 8-1
+SELECT COUNT(*)
+FROM payment;
+
+-- exercise 8-2
+SELECT customer_id, COUNT(*), SUM(amount)
+FROM payment
+GROUP BY customer_id;
+
+-- exercise 8-3
+SELECT customer_id, COUNT(*), SUM(amount)
+FROM payment
+GROUP BY customer_id
+HAVING COUNT(*) >= 40;
